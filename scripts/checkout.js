bd30e6c9115ad0069738b168ea67111aa7e819d2
Victorhf <victorhferreira02@gmail.com>
2026-04-15 13:27:23 -0300

    Using async await in other cases

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,23 +2,11 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
-// import "../data/cart-oop.js";
-// import "../data/backend-practice.js";
+import { loadCartFetch } from "../data/cart.js";
 
 async function loadPage() {
   try {
-    // throw "error1";
-
-    await loadProductsFetch();
-
-    const value = await new Promise((resolve, reject) => {
-      // throw "error2";
-      loadCart(() => {
-        // reject("error3");
-        resolve("value3");
-      });
-    });
+    await Promise.all([loadProductsFetch(), loadCartFetch()]);
   } catch (error) {
     console.log("Unexpected error. Please try again later");
   }
